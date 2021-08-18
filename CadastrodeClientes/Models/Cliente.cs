18add545c201026e0c12e5655f198f7c989e75a5
Alexandrade01 <solidwinter96@outlook.com>
2021-08-18 16:37:34 -0300

    Criacao classe Cliente

---
+++ CadastrodeClientes/Models/Cliente.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CadastrodeClientes.Models
+{
+
+    [Table("Clientes")]
+    public class Cliente
+    {
+        [Display(Name = "Id")]
+        [Column("ID")]
+        public int Id { get; set; }
+
+        [Display(Name = "Nome")]
+        [Column("Nome")]
+        public string Nome { get; set; }
+
+        [Display(Name = "Telefone")]
+        [Column("Telefone")]
+        public int Telefone { get; set; }
+
+        [Display(Name = "Email")]
+        [Column("Email")]
+        public string Email { get; set; }
+    }
+}
