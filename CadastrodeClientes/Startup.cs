631310fd8c4d853e0e0065fe3e97c37315df9fad
Alexandrade01 <solidwinter96@outlook.com>
2021-08-18 16:53:04 -0300

    Criacao de pacotes Mysql.Data e criacao de string de conexao

--- CadastrodeClientes/Startup.cs
+++ CadastrodeClientes/Startup.cs
@@ -1,6 +1,8 @@
+using CadastrodeClientes.Models;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -23,6 +25,8 @@ namespace CadastrodeClientes
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            string stringConexao = "Server=localhost;DataBase=WebDataClientes;Uid=developer;Pwd=1234567";
+            services.AddDbContext<Contexto>(options => options.UseMySQL(stringConexao));
             services.AddControllersWithViews();
         }
 
